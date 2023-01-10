085fe6d499bc9e3bb30e4d49980ae5bd3375990c
Srihari Ayapilla <srihariash@gmail.com>
2023-01-10 21:53:26 +0530

    added which env

--- routes/telegram.ts
+++ routes/telegram.ts
@@ -8,11 +8,20 @@ router.post("/", async (req: any, res: any) => {
   let bot_key = config.get("bot_api_key");
   let chat_id = config.get("chat_id");
 
+  let which_env = config.get("which_env");
+
   const telegram = new Telegram(bot_key);
-  let dt = new Date().toLocaleString();
-  console.log(dt.toString());
-  let msg = "Automated message from API call at  -  " + dt.toString();
-  telegram.send(chat_id, msg, Telegram_ParseModes.MarkdownV2);
+  var r: boolean = await telegram.send(
+    chat_id,
+    "Hi there, this is an automated text from " + which_env,
+    Telegram_ParseModes.MarkdownV2
+  );
+  // var r = await telegram.send(
+  //   chat_id,
+  //   which_env,
+  //   Telegram_ParseModes.MarkdownV2
+  // );
+  console.log(" status " + r);
   res.send("Some response here yooo");
 });
 
